825b6275524ff14386595c6eaf1c3a5a8591e10a
Arcadi del Castillo <98622132+Arcadi-dCC@users.noreply.github.com>
2024-04-18 18:06:56 +0200

    minor update in description

--- lib/collection/collectionCfg.h
+++ lib/collection/collectionCfg.h
@@ -8,6 +8,6 @@ extern float64 bins[TOTAL_BINS][6U];
 
 #define MIN_FULLNESS    50U //%
 
-#define COL_DONE_BTN    38 //IO number in ESP32 board
+#define COL_DONE_BTN    38 //IO pin in ESP32 board
 
 #endif //collectionCfg_H
